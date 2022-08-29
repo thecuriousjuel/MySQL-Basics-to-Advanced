cab7f0b755e384d6dd1d1263d0f42be8aa02f930
Biswajit Basak <juelbasak@gmail.com>
2022-08-29 15:07:00 +0530

    CASE operator on MySQL

--- 120.sql
+++ 120.sql
@@ -3,7 +3,7 @@ SELECT
     points,
     CASE
         WHEN points > 3000 THEN 'Gold'
-        WHEN points >= 2000 AND points <= 3000 THEN 'Silver'
+        WHEN points >= 2000 THEN 'Silver'
         ELSE 'Bronze'
     END AS category
 FROM customers
